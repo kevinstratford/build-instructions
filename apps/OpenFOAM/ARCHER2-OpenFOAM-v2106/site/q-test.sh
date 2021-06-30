372b6d463bdb47d06aa12723f4e6b8b20670185e
Kevin Stratford <kevin@epcc.ed.ac.uk>
2021-06-30 11:18:07 +0100

    Add OpenFOAM v2106

---
+++ apps/OpenFOAM/ARCHER2-OpenFOAM-v2106/site/q-test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash --login
+
+#SBATCH --nodes=1
+#SBATCH --exclusive
+#SBATCH --time=00:10:00
+
+#SBATCH --partition=standard
+#SBATCH --qos=standard
+#SBATCH --export=none
+
+set | grep SLURM
+
+printf "Start: %s\n" "`date`"
+
+# Replaces, e.g.,
+# module load openfoam/com/version
+
+source ./site/version.sh
+source ./site/modules.sh
+
+export FOAM_INSTALL_PATH=`pwd`/OpenFOAM-${version}
+
+# Run test
+
+source ${FOAM_INSTALL_PATH}/etc/bashrc
+
+source ./site/test.sh
+
+printf "Finish: %s\n" "`date`"
